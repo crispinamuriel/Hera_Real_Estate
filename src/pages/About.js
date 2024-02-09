882cd1bc94b6383ebbc2f12cd7a44d6108e7bfb4
crispinamuriel <crispinamuriel@gmail.com>
2024-02-08 22:00:41 -0500

    add styles to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,74 +2,84 @@ import './styles/About.css';
 
 export default function About() {
   return (
-    <div className="about">
-      <div>
-        <h2>Welcome to Hera's Dream Dwellings</h2>
-        <p>
-          At Hera's Dream Dwellings, we believe that finding the perfect home is
-          not just about the property; it's about discovering the place where
-          your dreams take root and flourish. Established in [year], our journey
-          began with a passion for helping individuals and families find their
-          ideal homes, and it continues with an unwavering commitment to
-          excellence in real estate services.
-        </p>
-      </div>
-      <div>
-        <h2>Our mission</h2>
-        <p>
-          Our mission is simple yet profound: to guide and support you through
-          every step of your real estate journey. We strive to create meaningful
-          connections between people and properties, transforming houses into
-          homes and aspirations into realities.
-        </p>
-      </div>
-      <div>
-        <h2>A Legacy of Trust and Expertise</h2>
-        <p>
-          With over 10 years of experience, Hera's Dream Dwellings has been a
-          cornerstone in the real estate industry. Our team comprises dedicated
-          professionals with extensive knowledge and a shared commitment to
-          providing personalized solutions that cater to your unique needs.
-        </p>
-      </div>
-      <div>
-        <h2>Meet Our Team</h2>
-        <p>
-          Behind Hera's Dream Dwellings stands a team of passionate individuals
-          driven by a collective vision: to redefine real estate by placing your
-          satisfaction at the forefront. Our experts bring a wealth of knowledge
-          and an unwavering dedication to delivering unparalleled service.
-        </p>
-      </div>
-      <div>
-        <h2>Client-Centric Excellence</h2>
-        <p>
-          Your satisfaction is our priority. We take pride in going above and
-          beyond to exceed your expectations. Every client interaction is an
-          opportunity for us to showcase our commitment to providing tailored
-          solutions and unparalleled support.
-        </p>
-      </div>
-      <div>
-        <h2>Building Communities Togethere</h2>
-        <p>
-          Beyond real estate transactions, we are deeply invested in supporting
-          the communities we serve. Through various initiatives and
-          partnerships, we strive to contribute positively to the neighborhoods
-          and cities we call home.
-        </p>
-      </div>
-      <div>
-        <h2>Looking to the Future</h2>
-        <p>
-          As we move forward, Hera's Dream Dwellings remains committed to
-          innovation, growth, and continuing to be a trusted partner in your
-          real estate endeavors. Our vision is to expand our services while
-          maintaining the personalized touch that defines us. Thank you for
-          considering Hera's Dream Dwellings for your real estate needs. Join us
-          on this journey, and let's turn your dreams into addresses together.
-        </p>
-      </div>
+    <div className="container">
+      <img
+        className="img marginAll"
+        src="https://images.unsplash.com/photo-1571897767174-35e817232fba?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="painted ladies in SF"
+      />
+      <section className="content">
+        <div>
+          <h2>Welcome to Hera's Dream Dwellings</h2>
+          <p>
+            At Hera's Dream Dwellings, we believe that finding the perfect home
+            is not just about the property; it's about discovering the place
+            where your dreams take root and flourish. Established in 2024, our
+            journey began with a passion for helping individuals and families
+            find their ideal homes, and it continues with an unwavering
+            commitment to excellence in real estate services.
+          </p>
+        </div>
+        <div>
+          <h2>Our mission</h2>
+          <p>
+            Our mission is simple yet profound: to guide and support you through
+            every step of your real estate journey. We strive to create
+            meaningful connections between people and properties, transforming
+            houses into homes and aspirations into realities.
+          </p>
+        </div>
+        <div>
+          <h2>A Legacy of Trust and Expertise</h2>
+          <p>
+            With over 10 years of experience, Hera's Dream Dwellings has been a
+            cornerstone in the real estate industry. Our team comprises
+            dedicated professionals with extensive knowledge and a shared
+            commitment to providing personalized solutions that cater to your
+            unique needs.
+          </p>
+        </div>
+        <div>
+          <h2>Meet Our Team</h2>
+          <p>
+            Behind Hera's Dream Dwellings stands a team of passionate
+            individuals driven by a collective vision: to redefine real estate
+            by placing your satisfaction at the forefront. Our experts bring a
+            wealth of knowledge and an unwavering dedication to delivering
+            unparalleled service.
+          </p>
+        </div>
+        <div>
+          <h2>Client-Centric Excellence</h2>
+          <p>
+            Your satisfaction is our priority. We take pride in going above and
+            beyond to exceed your expectations. Every client interaction is an
+            opportunity for us to showcase our commitment to providing tailored
+            solutions and unparalleled support.
+          </p>
+        </div>
+        <div>
+          <h2>Building Communities Togethere</h2>
+          <p>
+            Beyond real estate transactions, we are deeply invested in
+            supporting the communities we serve. Through various initiatives and
+            partnerships, we strive to contribute positively to the
+            neighborhoods and cities we call home.
+          </p>
+        </div>
+        <div>
+          <h2>Looking to the Future</h2>
+          <p>
+            As we move forward, Hera's Dream Dwellings remains committed to
+            innovation, growth, and continuing to be a trusted partner in your
+            real estate endeavors. Our vision is to expand our services while
+            maintaining the personalized touch that defines us. Thank you for
+            considering Hera's Dream Dwellings for your real estate needs. Join
+            us on this journey, and let's turn your dreams into addresses
+            together.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
